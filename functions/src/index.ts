2d59daa335ea410195a958b1b696197dceff3cda
Manor <manor.ben-shimon@vcita.com>
2020-08-20 18:09:49 +0300

    managed to get code and access token

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ require('dotenv').config();
 // import * as express from 'express';
 // const app = express();
 const axios = require('axios').default;
+// const cors = require('cors')({ origin: true });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -17,13 +18,36 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const authorize = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.json({
-      params: request.params,
-      body: request.body,
-      query: request.query
-  });
+export const authorize = functions.https.onRequest( async (request, response) => {
+  const code = request.query.code;
+  const config1 = {
+    method: 'POST',
+    url: 'https://api.vcita.biz/oauth/token',
+    params: {
+      grant_type: 'authorization_code', 
+      code: code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      redirect_uri: 'https://us-central1-vcita-playground.cloudfunctions.net/authorize'
+    }
+  }
+
+  const accessToken = await axios(config1);
+  response.json({code: code, token: accessToken.data.access_token});
+  // const config2 = {
+  //   method: 'GET',
+  //   url: 'https://api.vcita.biz/oauth/userinfo',
+  //   headers: {
+  //     'Content-Type':  'application/json',
+  //     Authorization: `Bearer ${token}`
+  //   }
+  // }
+
+  // response.json({
+  //     params: request.params,
+  //     body: request.body,
+  //     query: request.query
+  // });
 });
 
 export const vcitaClientCreatedWebhook = functions.https.onRequest((request, response) => {
@@ -35,38 +59,18 @@ export const vcitaClientCreatedWebhook = functions.https.onRequest((request, res
     });
   });
   
-export const getAuthCode = functions.https.onRequest((request, response) => {
+export const getAuthCode = functions.https.onRequest(async (request, response) => {
 
-  // const config = {
-  //   method: 'GET',
-  //   url: 'http://app.vcita.com/app/oauth/authorize',
-  //   params: {
-  //     client_id: process.env.CLIENT_ID,
-  //     redirect_uri: 'https://us-central1-vcita-playground.cloudfunctions.net/authorize',
-  //     state: 'TEST'
-  //   }
-  // }
-
-  function test() {
-    return new Promise((resolve, reject) => {
-      axios('https://jsonplaceholder.typicode.com/todos/1')
-      .then((res: any) => {
-        resolve(res)   
-      })
-      .catch((err: any) => {
-        reject(err)   
-      })
-    })
+  const config = {
+    method: 'GET',
+    url: 'http://app.vcita.com/app/oauth/authorize',
+    params: {
+      client_id: process.env.CLIENT_ID,
+      redirect_uri: 'https://us-central1-vcita-playground.cloudfunctions.net/authorize',
+      state: 'TEST'
+    }
   }
 
-  test()
-  .then(res => {
-    functions.logger.info(res, {structuredData: true})
-    response.json(res)
-  })
-  .catch(err => {
-    functions.logger.info(err, {structuredData: true})
-    response.send(err)
-  })
-
+  const myRes = await axios(config);
+  response.send(myRes.data)
 })
